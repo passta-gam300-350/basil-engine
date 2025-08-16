93fa5ab1334d9dcd60e919c2b42764c332b3a953
unknown <stevenchewbx@gmail.com>
2025-08-16 19:24:38 +0800

    updated folders

--- cmake/import-dependencies.cmake
+++ cmake/import-dependencies.cmake
@@ -146,5 +146,5 @@ macro(import_dependencies)
     import_rttr()
     import_imgui()
 
-    set_target_properties(glad glfw glm rttr_core PROPERTIES FOLDER dep)
+    set_target_properties(glad glfw glm rttr_core assimp EnTT imgui UpdateAssimpLibsDebugSymbolsAndDLLs zlibstatic PROPERTIES FOLDER dep)
 endmacro()
