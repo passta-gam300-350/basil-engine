44b8e824269b4d6b30356c61b08dbb87081b3d88
Tham Kang Ting <thamkangting@gmail.com>
2025-08-26 19:09:12 +0800

    remove old code

--- lib/Graphics/include/Core/RenderCommand.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-// All command types now use the efficient value-based system
-#include "RenderCommandBuffer.h"
-
-// Export the new command types
-using RenderCommand = VariantRenderCommand;
-namespace Commands = RenderCommands;
