186f24f9d40a2a334fd6c60c8406eaca46879c9b
Tham Kang Ting <thamkangting@gmail.com>
2025-08-26 17:36:04 +0800

    minor

--- lib/Graphics/src/Core/RenderQueue.cpp
+++ lib/Graphics/src/Core/RenderQueue.cpp
@@ -13,14 +13,14 @@ RenderQueue::~RenderQueue()
 void RenderQueue::Submit(const RenderCommand& command)
 {
 	// Clone the command and add it to the queue
-	std::cout << "RenderQueue: Command submitted, queue size now: " << (m_Commands.size() + 1) << std::endl;
+	//std::cout << "RenderQueue: Command submitted, queue size now: " << (m_Commands.size() + 1) << std::endl;
 	m_Commands.emplace_back(std::unique_ptr<RenderCommand>(command.Clone()));
 }
 
 void RenderQueue::Execute()
 {
 	// Execute all commands in the queue
-	std::cout << "RenderQueue: Executing " << m_Commands.size() << " commands" << std::endl;
+	//std::cout << "RenderQueue: Executing " << m_Commands.size() << " commands" << std::endl;
 	for (const auto& command : m_Commands)
 	{
 		command->Execute();
