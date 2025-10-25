6146dd47c1470f7f68329bae21f29d50ab917a9e
FriedTofu86 <jiazencheong@gmail.com>
2025-10-26 04:21:13 +0800

    Particle Renderer done

--- lib/Graphics/src/Rendering/ParticleRenderer.cpp
+++ lib/Graphics/src/Rendering/ParticleRenderer.cpp
@@ -24,6 +24,19 @@ void ParticleRenderer::SubmitParticleSystem(ParticleRenderData const& dataNeeded
 
 void ParticleRenderer::RenderToPass(RenderPass& passData, FrameData const& frameData)
 {
+	if (m_SubmittedSystems.empty() || !m_ParticleShader)
+	{
+		return;
+	}
+	m_TotalParticleCount = 0;
+	for (auto const& eachSystem : m_SubmittedSystems)
+	{
+		if (eachSystem.particles.empty() == false)
+		{
+			RenderSystem(eachSystem, frameData);
+			m_TotalParticleCount += eachSystem.particles.size();
+		}
+	}
 }
 
 void ParticleRenderer::ClearFrame()
@@ -60,17 +73,90 @@ void ParticleRenderer::InitializeResources()
 
 void ParticleRenderer::CreateBillboardQuad()
 {
-	
+	Mesh quadMesh = PrimitiveGenerator::CreateFullscreenQuad();
+	m_QuadVAO = quadMesh.GetVertexArray();
 }
 
 void ParticleRenderer::RenderSystem(ParticleRenderData const& system, FrameData const& frameData)
 {
+	UpdateSSBO(system.particles);
+	uint32_t activeCount = 0;
+	for (auto const& eachParticle : system.particles)
+	{
+		if (eachParticle.active == true)
+		{
+			activeCount++;
+		}
+	}
+	if (activeCount == 0)
+	{
+		return;
+	}
+	m_ParticleShader->use();
+	m_ParticleShader->setMat4("uProjection", frameData.projectionMatrix);
+	m_ParticleShader->setMat4("uView", frameData.viewMatrix);
+	if (system.texture)
+	{
+		glActiveTexture(GL_TEXTURE0);  
+		glBindTexture(GL_TEXTURE_2D, system.texture->id); 
+		m_ParticleShader->setInt("uTexture", 0);  
+	}
+	SetBlendMode(system.blendMode);
+	if (system.depthWrite == false) 
+	{
+		glDepthMask(GL_FALSE);
+	}
+	m_QuadVAO->Bind();
+	glDrawElementsInstanced(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0, activeCount);
+	if (system.depthWrite == false)
+	{
+		glDepthMask(GL_TRUE);
+	}
+	glDisable(GL_BLEND);
 }
 
 void ParticleRenderer::UpdateSSBO(std::vector<Particle> const& particles)
 {
+	std::vector<ParticleInstanceData> instanceParticleData;
+	instanceParticleData.reserve(particles.size());
+	for (auto const& eachParticle : particles)
+	{
+		if (eachParticle.active == true)
+		{
+			ParticleInstanceData eachParticleData;
+			eachParticleData.color = eachParticle.color;
+			eachParticleData.position = eachParticle.position;
+			eachParticleData.rotation = eachParticle.rotation;
+			eachParticleData.size = eachParticle.size;
+			eachParticleData.textureID = 0; // havent implemented texture arrays yet, no worries
+			eachParticleData.padding[0] = 0;
+			eachParticleData.padding[1] = 0;
+			instanceParticleData.push_back(eachParticleData);
+		}
+	}
+	if (instanceParticleData.empty() == false)
+	{
+		m_InstanceSSBO->SetData(instanceParticleData.data(), instanceParticleData.size() * sizeof(ParticleInstanceData));
+	}
 }
 
 void ParticleRenderer::SetBlendMode(BlendMode mode)
 {
+	glEnable(GL_BLEND);
+	if (mode == BlendMode::Alpha)
+	{
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	}
+	else if (mode == BlendMode::Additive)
+	{
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE);
+	}
+	else if (mode == BlendMode::Multiply)
+	{
+		glBlendFunc(GL_DST_COLOR, GL_ZERO);
+	}
+	else if (mode == BlendMode::Opaque)
+	{
+		glDisable(GL_BLEND);
+	}
 }
