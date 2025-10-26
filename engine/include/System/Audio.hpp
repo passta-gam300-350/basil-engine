1cda7c53f0498c9219c6e18592ab00626c6017e1
Zandroid01 <personal@halisilyasa.com>
2025-10-26 16:04:29 +0800

    temp

--- engine/include/System/Audio.hpp
+++ engine/include/System/Audio.hpp
@@ -3,10 +3,10 @@
 //#include <cassert>
 //#include <unordered_map>
 //#include <vector>
-//#include "fmod.hpp"
-//#include "fmod_errors.h"
+//#include "../../vendor/fmod/api/core/inc/fmod.hpp"
+//#include "../../vendor/fmod/api/core/inc/fmod_errors.h"
 //
-//#include "../examples/common.h" // TEMP
+////#include "../examples/common.h" // TEMP
 //
 //#define AUDIO_EXTENSION ".ogg"
 //#define DOPPLERSCALE 1.0f
@@ -55,3 +55,58 @@
 //	void Exit();
 //#pragma endregion
 //}
+//
+////
+//
+//#ifndef ENGINE_AUDIO_H
+//#define ENGINE_AUDIO_H
+//#pragma once
+//
+//#include "Ecs/ecs.h"
+//
+//struct AudioComponent
+//{
+//	enum class Sound_Mode : std::uint8_t
+//	{
+//		LOOP,
+//		NO_LOOP
+//	}m_Mode;
+//	enum class Filter : std::uint8_t
+//	{
+//		LOW_PASS,
+//		HIGH_PASS,
+//		ECHO,
+//		DISTORTION,
+//		SIZE_OF_FILTERS
+//	}m_Filter;
+//	bool m_IsActive;
+//	bool m_3D;
+//	float m_Volume;
+//	std::string m_SoundName;
+//	std::vector<Filter> m_Filters;
+//};
+//
+//struct AudioSystem : public ecs::SystemBase
+//{
+//public:
+//	static void Play_Audio();
+//	static void Stop_Audio();
+//	static void Stop_All_Audio();
+//	static void Load_Audio(std::string _dir = "", bool _loop, bool _stream, bool _3d, bool _linear = false);
+//	static void Unload_Audio();
+//	static void Unload_All_Audio();
+//
+//	static FMOD_MODE Mode_Selector(Sound_Mode _mode);
+//	static FMOD_DSP_TYPE Filter_Selector(Filter _filter);
+//private:
+//	//static std::unique_ptr<InstanceData& InstancePtr();
+//public:
+//	//static InstanceData& Instance();
+//	//static AudioSystem System();
+//	//~AudioSystem() = default;
+//
+//	void Init();
+//	void Update();
+//	void Exit();
+//};
+//#endif
