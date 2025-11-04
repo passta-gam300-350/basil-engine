959525bbcc5831bc68fe46cb53ff6e922ddd8347
FriedTofu86 <jiazencheong@gmail.com>
2025-11-04 11:05:42 +0800

    Fix Frustum Ctor problem

--- engine/src/BVH/bvh_helper.cpp
+++ engine/src/BVH/bvh_helper.cpp
@@ -4,55 +4,20 @@
 
 Frustum CameraToFrustum(CameraSystem::Camera const& camera)
 {
-    Frustum frustum;
-
-    // extract camera data
-    glm::vec3 pos = camera.m_Pos;
-    glm::vec3 front = camera.m_Front;
-    glm::vec3 right = camera.m_Right;
-    glm::vec3 up = camera.m_Up;
-
-    float nearDist = camera.m_Near;
-    float farDist = camera.m_Far;
-    float fov = camera.m_Fov;          // in degrees
-    float aspect = camera.m_AspectRatio;
-
-    // calculate frustum dimensions
-    float tanHalfFov = tan(glm::radians(fov / 2.0f));
-    float nearHeight = 2.0f * tanHalfFov * nearDist;
-    float nearWidth = nearHeight * aspect;
-    float farHeight = 2.0f * tanHalfFov * farDist;
-    float farWidth = farHeight * aspect;
-
-    // centers of near and far planes
-    glm::vec3 nearCenter = pos + front * nearDist;
-    glm::vec3 farCenter = pos + front * farDist;
-
-    // === construct 6 planes (normals point INWARD) ===
-
-    // near plane (points forward, towards far)
-    frustum.planes[0] = Plane(front, glm::dot(front, nearCenter));
-
-    // far plane (points backward, towards near)
-    frustum.planes[1] = Plane(-front, glm::dot(-front, farCenter));
-
-    // left plane
-    glm::vec3 leftNormal = glm::normalize(glm::cross(up, front * farDist - right * (farWidth / 2.0f)));
-    frustum.planes[2] = Plane(leftNormal, glm::dot(leftNormal, pos));
-
-    // rright plane
-    glm::vec3 rightNormal = glm::normalize(glm::cross(front * farDist + right * (farWidth / 2.0f), up));
-    frustum.planes[3] = Plane(rightNormal, glm::dot(rightNormal, pos));
-
-    // top plane
-    glm::vec3 topNormal = glm::normalize(glm::cross(right, front * farDist + up * (farHeight / 2.0f)));
-    frustum.planes[4] = Plane(topNormal, glm::dot(topNormal, pos));
-
-    // bottom plane
-    glm::vec3 bottomNormal = glm::normalize(glm::cross(front * farDist - up * (farHeight / 2.0f), right));
-    frustum.planes[5] = Plane(bottomNormal, glm::dot(bottomNormal, pos));
-
-    return frustum;
+    glm::mat4 projection = glm::perspective
+    (
+        glm::radians(camera.m_Fov),
+        camera.m_AspectRatio,
+        camera.m_Near,
+        camera.m_Far
+    );
+    glm::mat4 view = glm::lookAt
+    (
+        camera.m_Pos,
+        camera.m_Pos + camera.m_Front,
+        camera.m_Up
+    );
+    return Frustum(projection * view);
 }
 
 Aabb TransformAABB(Aabb const& localAABB, glm::mat4 const& matrix)
