6b6a75f759072992c36a4bb1dd764d116d0c54d6
Yeo Jia Hao <jiahao.yeo@digipen.edu>
2025-11-17 09:28:05 +0800

    Changes

--- engine/managed/BasilEngine/Components/Transform.cs
+++ engine/managed/BasilEngine/Components/Transform.cs
@@ -9,13 +9,13 @@ using Engine.Bindings;
 namespace BasilEngine.Components
 {
 
-    
+
 
     [NativeHeader("Bindings/ManagedTransform.hpp")]
     [NativeClass("ManagedTransform")]
     public class Transform : Component
     {
-       
+
         [MethodImpl(MethodImplOptions.InternalCall)]
         [NativeMethod("SetPosition")]
         [StaticAccessor("ManagedTransform", StaticAccessorType.DoubleColon)]
@@ -26,7 +26,42 @@ namespace BasilEngine.Components
         [NativeMethod("GetPosition")]
         [StaticAccessor("ManagedTransform", StaticAccessorType.DoubleColon)]
 
-        public static  extern void GetPosition(UInt64 handle, out float x, out float y, out float z);
+        public static extern void GetPosition(UInt64 handle, out float x, out float y, out float z);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        [NativeMethod("SetRotation")]
+        [StaticAccessor("ManagedTransform", StaticAccessorType.DoubleColon)]
+        public extern static void SetRotationInternal(UInt64 handle, float x, float y, float z);
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        [NativeMethod("GetRotation")]
+        [StaticAccessor("ManagedTransform", StaticAccessorType.DoubleColon)]
+        public extern static void GetRotationInternal(UInt64 handle, out float x, out float y, out float z);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        [NativeMethod("SetScale")]
+        [StaticAccessor("ManagedTransform", StaticAccessorType.DoubleColon)]
+        public extern static void SetScaleInternal(UInt64 handle, float x, float y, float z);
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        [NativeMethod("GetScale")]
+		[StaticAccessor("ManagedTransform", StaticAccessorType.DoubleColon)]
+		public extern static void GetScaleInternal(UInt64 handle, out float x, out float y, out float z);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		[NativeMethod("GetForward")]
+		[StaticAccessor("ManagedTransform", StaticAccessorType.DoubleColon)]
+		public extern static void GetForwardInternal(UInt64 handle, out float x, out float y, out float z);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		[NativeMethod("GetRight")]
+		[StaticAccessor("ManagedTransform", StaticAccessorType.DoubleColon)]
+		public extern static void GetRightInternal(UInt64 handle, out float x, out float y, out float z);
+
+		[MethodImpl(MethodImplOptions.InternalCall)]
+		[NativeMethod("GetUp")]
+		[StaticAccessor("ManagedTransform", StaticAccessorType.DoubleColon)]
+		public extern static void GetUpInternal(UInt64 handle, out float x, out float y, out float z);
+
+
 
         public Vector3 position
         {
@@ -42,19 +77,63 @@ namespace BasilEngine.Components
         }
         public Vector3 rotation
         {
-            get;
-            set;
-        } = Vector3.Zero;
+            get
+            {
+                GetRotationInternal(NativeID, out float x, out float y, out float z);
+                return new Vector3(x, y, z);
+            }
+            set
+            {
+                SetRotationInternal(NativeID, value.x, value.y, value.z);
+            }
+        }
+
         public Vector3 scale
         {
-            get;
-            set;
-        } = new Vector3(1, 1, 1);
+            get
+            {
+                GetScaleInternal(NativeID, out float x, out float y, out float z);
+                return new Vector3(x, y, z);
+            }
+            set
+            {
+                SetScaleInternal(NativeID, value.x, value.y, value.z);
+            }
+        }
+
 
         public Transform(UInt64 handle)
         {
             NativeID = handle; // GameObject Handle
         }
+
+
+		public Vector3 forward
+		{
+			get
+			{
+				GetForwardInternal(NativeID, out float x, out float y, out float z);
+				return new Vector3(x, y, z);
+			}
+		}
+
+		public Vector3 right
+		{
+			get
+			{
+				GetRightInternal(NativeID, out float x, out float y, out float z);
+				return new Vector3(x, y, z);
+			}
+		}
+
+		public Vector3 up
+		{
+			get
+			{
+				GetUpInternal(NativeID, out float x, out float y, out float z);
+				return new Vector3(x, y, z);
+			}
+		}
 
     }
 }
--- engine/managed/BasilEngine/Mathematics/Vector3.cs
+++ engine/managed/BasilEngine/Mathematics/Vector3.cs
@@ -35,5 +35,89 @@ namespace BasilEngine.Mathematics
         {
             return new Vector3(a.x + b.x, a.y + b.y, a.z + b.z);
         }
+
+        public static Vector3 operator -(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.x - b.x, a.y - b.y, a.z - b.z);
+        }
+
+        public static Vector3 operator *(Vector3 a, float scalar)
+        {
+            return new Vector3(a.x * scalar, a.y * scalar, a.z * scalar);
+        }
+        public static Vector3 operator /(Vector3 a, float scalar)
+        {
+            return new Vector3(a.x / scalar, a.y / scalar, a.z / scalar);
+        }
+
+        public static Vector3 operator *(float scalar, Vector3 a)
+        {
+            return new Vector3(a.x * scalar, a.y * scalar, a.z * scalar);
+        }
+
+        public static Vector3 operator -(Vector3 a)
+        {
+            return new Vector3(-a.x, -a.y, -a.z);
+        }
+        
+        public static Vector3 operator ++(Vector3 a)
+        {
+            return new Vector3(a.x + 1, a.y + 1, a.z + 1);
+        }
+        public static Vector3 operator --(Vector3 a)
+        {
+            return new Vector3(a.x - 1, a.y - 1, a.z - 1);
+        }
+
+        // Dot product
+        public static float Dot(Vector3 a, Vector3 b)
+        {
+            return a.x * b.x + a.y * b.y + a.z * b.z;
+        }
+
+        // Cross product
+        public static Vector3 Cross(Vector3 a, Vector3 b)
+        {
+            return new Vector3(
+                a.y * b.z - a.z * b.y,
+                a.z * b.x - a.x * b.z,
+                a.x * b.y - a.y * b.x
+            );
+        }
+        // Magnitude
+        public float Magnitude()
+        {
+            return Mathf.Sqrt(x * x + y * y + z * z);
+        }
+
+        public float MagnitudeSqr()
+        {
+            return x * x + y * y + z * z;
+        }
+        // Normalize
+        public Vector3 Normalize()
+        {
+            float mag = Magnitude();
+            if (mag > 0)
+            {
+                return this / mag;
+            }
+            return Vector3.Zero;
+        }
+        // Distance
+        public static float Distance(Vector3 a, Vector3 b)
+        {
+            return (b - a).Magnitude();
+        }
+        // Lerp
+        public static Vector3 Lerp(Vector3 a, Vector3 b, float t)
+        {
+            t = Mathf.Clamp(t, 0f, 1f);
+            return new Vector3(
+                a.x + (b.x - a.x) * t,
+                a.y + (b.y - a.y) * t,
+                a.z + (b.z - a.z) * t
+            );
+        }
     }
 }
