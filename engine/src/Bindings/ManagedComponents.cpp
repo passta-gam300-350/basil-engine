2280c9374534cd8a7078b99ee17f7bd8f7b6dcb3
Yeo Jia Hao <jiahao.yeo@digipen.edu>
2026-02-01 12:52:50 +0800

    Update

--- engine/src/Bindings/ManagedComponents.cpp
+++ engine/src/Bindings/ManagedComponents.cpp
@@ -24,6 +24,7 @@ Technology is prohibited.
 #include "components/behaviour.hpp"
 #include "components/transform.h"
 #include "Manager/MonoEntityManager.hpp"
+#include "Particles/ParticleComponent.h"
 #include "Physics/Physics_Components.h"
 #include "Render/Camera.h"
 #include "System/Audio.hpp"
@@ -126,6 +127,22 @@ uint32_t ManagedComponents::ManagedRegisterComponent(MonoString* name)
 		}, [](ecs::entity& entity)
 		{
 			entity.remove<VideoComponent>();
+		});
+		return id;
+	}
+
+	if (strName == "Particle")
+	{
+		componentMapped[strName] = 1000000 + registerCount++; // ID for Particle component
+		unsigned id = componentMapped[strName];
+		RegisterComponentType(id, [](ecs::entity const& entity)
+		{
+			// Placeholder for Particle component check
+			bool result = entity.any<ParticleComponent>(); // Replace with actual Particle component
+			return result;
+		}, [](ecs::entity& entity)
+		{
+			entity.remove<ParticleComponent>();
 		});
 		return id;
 	}
