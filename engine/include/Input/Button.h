1ca1b1df9488e78d475d4894593e9e651dfdd3dc
aaron <aaronnic1998@gmail.com>
2026-02-01 14:23:15 +0800

    button

---
+++ engine/include/Input/Button.h
@@ -0,0 +1,15 @@
+/*!************************************************************************
+\file:      Button.h
+\author:    Saminathan Aaron Nicholas
+\email:     s.aaronnicholas@digipen.edu
+\course:    CSD 3451 - Software Engineering Project 6
+\brief:     This file implements the class for the button component.
+
+            Methods are also offered to query key and mouse states, fetching mouse positions, and retrieving scroll offsets.
+            It also resets input states at the end of each frame and handles input state persistence between frames.
+
+Copyright (C) 2026 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the
+prior written consent of DigiPen Institute of Technology is prohibited.
+**************************************************************************/
+
---
+++ engine/src/Input/Button.cpp
@@ -0,0 +1,15 @@
+/*!************************************************************************
+\file:      Button.cpp
+\author:    Saminathan Aaron Nicholas
+\email:     s.aaronnicholas@digipen.edu
+\course:    CSD 3451 - Software Engineering Project 6
+\brief:     This file implements the class of the button and determines if the button is being hovered over, clicked or
+            not interacted with.
+
+            Methods are also offered to query key and mouse states, fetching mouse positions, and retrieving scroll offsets.
+            It also resets input states at the end of each frame and handles input state persistence between frames.
+
+Copyright (C) 2026 DigiPen Institute of Technology.
+Reproduction or disclosure of this file or its contents without the
+prior written consent of DigiPen Institute of Technology is prohibited.
+**************************************************************************/
