c87fc2208627b30cdac10af0370b477718ada780
aaron <aaronnic1998@gmail.com>
2026-03-08 16:34:54 +0800

    comments

--- engine/include/BehaviourTree/BehaviorTree.h
+++ engine/include/BehaviourTree/BehaviorTree.h
@@ -34,12 +34,46 @@ private:
 };
 
 /*
-Creating a tree for a character:
-    Class character
-    {
-        private:
-            BehaviorTree* tree;
-    }
+Create BehaviorAgent - Characters that use behaviour trees must be created as BehaviorAgent objects.
 
-Register new nodes in Nodes.def
+Example:
+auto agent = agents->create_behavior_agent("Man", BehaviorTreeTypes::Man);
+
+Parameters:
+Character name
+BehaviorTreeTypes enum value
+
+-------------------------------------------------------------------------------
+Create / Modify a Behaviour Tree file - .bht file
+
+Example tree structure:
+
+TREENAME(Man)
+TREENODE(Selector,0)
+TREENODE(Attack,1)
+TREENODE(Chase,1)
+TREENODE(Patrol,1)
+
+depth 0 = root node
+higher depth = children nodes
+
+-------------------------------------------------------------------------------
+Register nodes
+
+- Nodes.def
+- Nodeheaders.h
+
+Node files are found in ControlFlow / Decorator / Leaf folders depending on the type of node
+
+-------------------------------------------------------------------------------
+Behaviour Tree execution
+
+Every frame the agent runs:
+BehaviorAgent::update()
+
+which calls:
+tree.update(dt)
+
+which executes:
+rootNode->tick(dt)
 */
