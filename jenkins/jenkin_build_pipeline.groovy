968dfff9f62d8cdd4903d53d1797f1188687365c
Yeo Jia Hao <jiahao.yeo@digipen.edu>
2026-03-27 21:33:05 +0800

    change to sequential build

--- jenkins/jenkin_build_pipeline.groovy
+++ jenkins/jenkin_build_pipeline.groovy
@@ -28,48 +28,43 @@ pipeline {
             }
         }
 
-        stage('Build Matrix') {
-            matrix {
-                axes {
-                    axis {
-                        name 'BUILD_CONFIG'
-                        values 'Release', 'Debug'
+        stage('Build') {
+            agent {
+                label 'Windows Agent 01'
+            }
+
+            stages {
+                stage('Checkout') {
+                    steps {
+                        checkout scm
                     }
                 }
 
-                
-
-                agent {
-                    label 'Windows Agent 01'
+                stage('Install Mono Dependency') {
+                    steps {
+                        powershell '''
+                            if (-not (Test-Path "dep/vendor/mono")) {
+                                git clone https://github.com/Silver1713/MONO.git dep/vendor/mono
+                            }
+                        '''
+                    }
                 }
 
-                stages {
-                    stage('Checkout') {
-                        steps {
-                            checkout scm
-                        }
+                stage('Configure CMake') {
+                    steps {
+                        powershell 'cmake -S . -B build -A x64'
                     }
+                }
 
-                    stage('Install Mono Dependency') {
-                        steps {
-                            powershell '''
-                                if (-not (Test-Path "dep/vendor/mono")) {
-                                    git clone https://github.com/Silver1713/MONO.git dep/vendor/mono
-                                }
-                            '''
-                        }
+                stage('Build Release') {
+                    steps {
+                        powershell 'cmake --build build --config Release --parallel'
                     }
+                }
 
-                    stage('Configure CMake') {
-                        steps {
-                            powershell 'cmake -S . -B build -A x64'
-                        }
-                    }
-
-                    stage('Build') {
-                        steps {
-                            powershell "cmake --build build --config ${env:BUILD_CONFIG} --parallel"
-                        }
+                stage('Build Debug') {
+                    steps {
+                        powershell 'cmake --build build --config Debug --parallel'
                     }
                 }
             }
